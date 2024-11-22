2d9cb50b7c4fed0d39d3e3c81c19acd844d9c580
Felipe Bañados Schwerter <banadoss@ualberta.ca>
2024-11-22 18:14:26 +0000

    Base version

---
+++ generate_json.py
@@ -0,0 +1,55 @@
+import json
+data = []
+with open('rapidwords.txt') as f:
+    for line in f:
+        data.append(line.split(' ', 1))
+
+# Process
+
+rw = dict()
+for entry in data:
+    index = entry[0].split(".")
+    domain = entry[1]
+    hypernyms = []
+    current = rw
+    for k in range(0, len(index)):
+        hypernyms.append(".".join(index[0:k]))
+        current = current.setdefault("subindexes",dict())
+        current = current.setdefault(index[k], dict())
+    current["domain"] = domain.replace("\n","")
+    current["index"] = entry[0]
+    current["hypernyms"] = hypernyms
+    current["hypernyms"].remove("")
+
+# Process hyponyms
+
+def add_hyponyms(entry, top):
+    ans = [ top ]
+    for key, value in entry.get("subindexes",dict()).items():
+        ans.extend(add_hyponyms(value,top+"."+key))
+    entry["hyponyms"] = ans.copy()
+    entry["hyponyms"].remove(entry["index"])
+    return ans
+
+for key, value in rw["subindexes"].items():
+    add_hyponyms(value,key)
+
+flat = dict()
+
+def flatten_data (entry) :
+    flat[entry["index"]] = {
+        "domain" : entry["domain"],
+        "hypernyms" : entry["hypernyms"],
+        "hyponyms": entry["hyponyms"]
+    }
+    for value in entry.get("subindexes",dict()).values():
+        flatten_data(value)
+
+with open ("rapidwords-compact.json","w") as f:
+    json.dump(rw,f) #,indent="\t")
+
+for value in rw["subindexes"].values():
+    flatten_data(value)
+
+with open("rapidwords.json", "w") as f:
+    json.dump(flat,f) #,indent="\t")
